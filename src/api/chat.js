bd46651505c22a2c65d294b921e7d9c860bc200e
kayalpiriya09 <sdilakshana1@gmail.com>
2025-12-31 20:00:52 +0530

    clean

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,9 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({ baseURL: "http://localhost:5000/chats" });
-
-// export const getChats = () => API.get("/");
-// export const createChat = (sender, message) => API.post("/", { sender, message });
 
 
 
--- src/api/review.js
+++ src/api/review.js
@@ -1,18 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({ baseURL: "http://localhost:5000/reviews" });
-
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem("token");
-//   if (token) req.headers.Authorization = `Bearer ${token}`;
-//   return req;
-// });
-
-// export const addReview = (productId, rating, comment) =>
-//   API.post("/add", { productId, rating, comment });
-
-// export const getReviewsByProduct = (productId) =>
-//   API.get(`/product/${productId}`);
 
 
 import axios from "axios";
